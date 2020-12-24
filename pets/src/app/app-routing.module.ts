cca42aee5074191c18c3a25316bd2e79d448810d
Bruno Costa <brunocosta@Brunos-MacBook-Air.local>
2020-12-24 09:36:50 -0300

    menu navigation/installing bootstrap

--- pets/src/app/app-routing.module.ts
+++ pets/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// COMPONENTS
+import { HomeComponent } from './navigation/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
