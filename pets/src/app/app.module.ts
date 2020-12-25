cf6936013e32814928e0685db3aae8d4c08b1fa9
Bruno Costa <brunocosta@Brunos-MacBook-Air.local>
2020-12-25 19:31:57 -0300

    adding ngx toastr

--- pets/src/app/app.module.ts
+++ pets/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
 
 // MODULES
 import { NavigationModule } from './navigation/navigation.module';
@@ -16,7 +19,9 @@ import { NavigationModule } from './navigation/navigation.module';
     BrowserModule,
     AppRoutingModule,
     NavigationModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
