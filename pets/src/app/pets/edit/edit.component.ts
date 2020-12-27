28af378408a4cf9069b74d5eb4782ecab60ba1e1
Bruno Costa <brunocosta@Brunos-MacBook-Air.local>
2020-12-27 09:39:55 -0300

    confirm action before update a pet

--- pets/src/app/pets/edit/edit.component.ts
+++ pets/src/app/pets/edit/edit.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, ViewChildren, ElementRef, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControlName, AbstractControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { fromEvent, merge, Observable } from 'rxjs';
 
 import cloneDeep from 'lodash';
 import { ToastrService } from 'ngx-toastr';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import Swal from 'sweetalert2';
 
 import { Pet } from '../models/pet';
 import { PetService } from '../services/pet.service';
 import { DisplayMessage, GenericValidator, ValidationMessages } from 'src/app/utils/generic-form-validation';
-import { fromEvent, merge, Observable } from 'rxjs';
 import { OwnersService } from 'src/app/owners/services/owners.service';
 import { Owner } from 'src/app/owners/models/owner';
 
@@ -92,17 +93,29 @@ ngAfterViewInit(): void {
 
 handleSubmit() {
     if (this.form.valid) {
-        
-    const valueFinal = cloneDeep(this.form.value);
-    valueFinal.id = this.activatedRoute.snapshot.paramMap.get('id');
-        
-    this.petService.update(valueFinal)
-        .subscribe(
-        success => { 
-            this.processSuccess(success);
-            this.router.navigate(['pets/todos']); 
-        },
-        error => { this.processError(error) });
+
+        Swal.fire({
+            title: 'Atualizar pet?',
+            text: "Tem certeza que deseja atualizar este pet?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, atualizar!',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                const valueFinal = cloneDeep(this.form.value);
+                valueFinal.id = this.activatedRoute.snapshot.paramMap.get('id');
+                this.petService.update(valueFinal)
+                    .subscribe(
+                    success => { 
+                        this.processSuccess(success);
+                        this.router.navigate(['pets/todos']); 
+                    },
+                    error => { this.processError(error) });
+            }
+        })  
     }
 }
 
